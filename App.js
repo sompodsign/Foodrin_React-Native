52b17ce7287fd95290151a413b82ea2734952547
Shampad Sharkar <sompodsign@gmail.com>
2021-07-18 02:13:14 +0600

    added navigation, navigated headers and styles.

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import MealsNavigator from "./navigation/MealsNavigator";
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    const [fontLoaded, setFontLoaded] = useState(false)
+
+    if (!fontLoaded) {
+        return (<AppLoading
+            startAsync={fetchFonts}
+            onError={console.warn}
+            onFinish={() => setFontLoaded(true)}/>)
+    }
+    return <MealsNavigator/>
+
+}
+
